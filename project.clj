20d26b14e5514ad94cdb53650f1594ee6d72ffab
hiroaqii <hiroaqii@gmail.com>
2018-01-15 01:31:23 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject kabosu "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["Sonatype" "https://oss.sonatype.org/content/repositories/snapshots"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.worksap.nlp/sudachi "0.1.1-SNAPSHOT"]])
+
+   
