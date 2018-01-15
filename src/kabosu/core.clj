939099ff991fc4c7e98ecb603dbc9338970b5275
hiroaqii <hiroaqii@gmail.com>
2018-01-16 00:12:36 +0900

    Add tokenize

--- src/kabosu/core.clj
+++ src/kabosu/core.clj
@@ -1,6 +1,10 @@
-(ns kabosu.core)
+(ns kabosu.core
+  (:import [com.worksap.nlp.sudachi DictionaryFactory Tokenizer$SplitMode]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce tokenizer (.create (.create (DictionaryFactory.))))
+
+(defn tokenize
+  [s]
+  (map
+   (fn [token] [(.surface token) (.partOfSpeech token)])
+   (.tokenize tokenizer Tokenizer$SplitMode/C s)))
